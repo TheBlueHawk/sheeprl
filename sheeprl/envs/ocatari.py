8ef595b91ac1e0fa9f95629e5c91e7a620b13fc4
Kevin Blin <kblin@outlook.com>
2024-01-26 14:57:32 +0100

    fix oc_atari obs_mode, mulitencoder runs

--- sheeprl/envs/ocatari.py
+++ sheeprl/envs/ocatari.py
@@ -6,51 +6,38 @@ from gymnasium import spaces
 
 class OCAtariWrapper(gym.Wrapper):
     """OCAtari Environment that behaves like a gymnasium environment and passes env_check. Based on RAM, the observation space is object-centric.
-    More specifically it is a list position history informations of objects detected. No hud information.
+    More specifically it is a list position history informations of objects detected.
     The observation space is a vector where every object position history has a fixed place.
     If an object is not detected its information entries are set to 0.
     """
 
-    def __init__(self, id: str, obs_mode:str, render_mode:str) -> None:
-        self.ocatari_env = OCAtari(env_name=id, mode="vision", hud=True, obs_mode=obs_mode, render_mode=render_mode)
+    def __init__(self, id: str, render_mode:str) -> None:
+        self.ocatari_env = OCAtari(env_name=id, mode="revised", hud=True, obs_mode="ori", render_mode=render_mode)
         super().__init__(self.ocatari_env)
         self.reference_list = self._init_ref_vector()
-        self.current_vector = np.zeros(4 * len(self.reference_list), dtype=np.float32)
-        self.obs_mode = obs_mode
+        self.current_vector = np.zeros(4 * len(self.reference_list), dtype=np.uint8)
 
     @property
     def observation_space(self):
         # fix to include pixel observations
         vl = len(self.reference_list) * 4
-        if self.obs_mode == "dqn":
-            return spaces.Dict({
-                "rgb": spaces.Box(0, 255, (3, 64, 64), np.uint8),
-                "objects_position": spaces.Box(low=-(2**63), high=2**63 - 2, shape=(vl,), dtype=np.float32)
-            })
-        elif self.obs_mode == "ori":
-            return spaces.Dict({
-                "rgb": self.ocatari_env.observation_space,
-                "objects_position": spaces.Box(low=-(2**63), high=2**63 - 2, shape=(vl,), dtype=np.float32)
-            })
+        return spaces.Dict({
+            "rgb": self.ocatari_env.observation_space,
+            "objects_position": spaces.Box(low=0, high=255, shape=(vl,), dtype=np.uint8)
+        })
 
     @property
     def action_space(self):
         return self.ocatari_env.action_space
 
     def step(self, *args, **kwargs):
-        #print("step called")
         obs, reward, truncated, terminated, info = self.ocatari_env.step(*args, **kwargs)
-        #print("step done")
         converted_obs = self._convert_obs(obs)
-        #print("converted obs")
         return converted_obs, reward, truncated, terminated, info
 
     def reset(self, *args, **kwargs):
-        #print("reset called")
         obs, info = self.ocatari_env.reset(*args, **kwargs)
-        #print("reset done")
         converted_obs = self._convert_obs(obs)
-        #print("converted obs")
         return converted_obs, info
     
     def _convert_obs(self, rgb_obs):
@@ -61,15 +48,12 @@ class OCAtariWrapper(gym.Wrapper):
         }
 
     def render(self, *args, **kwargs):
-        #print("render called")
         return self.ocatari_env.render(*args, **kwargs)
 
     def close(self, *args, **kwargs):
-        #print("close called")
         return self.ocatari_env.close(*args, **kwargs)
 
     def _init_ref_vector(self):
-        #print("init ref vector")
         reference_list = []
         obj_counter = {}       
         for o in self.ocatari_env.max_objects:
@@ -78,13 +62,10 @@ class OCAtariWrapper(gym.Wrapper):
             obj_counter[o.category] += 1
         for k in list(obj_counter.keys()):
             reference_list.extend([k for i in range(obj_counter[k])])
-        #print("init ref vector done", reference_list)
         return reference_list
 
     def _obj2vec(self):
-        #print("obj2vec called")
         temp_ref_list = self.reference_list.copy()
-        #print("self.ocatari_env.objects", self.ocatari_env.objects)
         for o in self.ocatari_env.objects:  # populate out_vector with object instance
             if o.category not in temp_ref_list:
                 continue
@@ -94,6 +75,5 @@ class OCAtariWrapper(gym.Wrapper):
             self.current_vector[start : start + 4] = flat  # write the slice
             temp_ref_list[idx] = ""  # remove reference from reference list
         for i, d in enumerate(temp_ref_list):
-            if d != "":  # fill not populated category instances wiht 0.0's
-                self.current_vector[i * 4 : i * 4 + 4] = [0.0, 0.0, 0.0, 0.0]
-        #print("obj2vec done")
+            if d != "":  # fill not populated category instances wiht 0's
+                self.current_vector[i * 4 : i * 4 + 4] = [0, 0, 0, 0]
