ac74a016b6ccbb64bc77b129e0b31890e45b1ac6
Kevin Blin <kblin@outlook.com>
2024-02-22 12:08:47 +0100

    update viz

--- run_0.sh
+++ run_0.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-
-
-for seed in 0 1 2
-do
-    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=AssaultNoFrameskip-v4 fabric.devices="[0]" seed=$seed algo.train_every=2 algo.mlp_keys.decoder="[]"
-done
-
-python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc fabric.devices="[0]" seed=0 algo.train_every=8 algo.cnn_keys.decoder="[]" algo.cnn_keys.encoder="[]"
-python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc fabric.devices="[0]" seed=0 algo.train_every=8 algo.mlp_keys.decoder="[]"
+python logs_processing.py Assault 0
+python logs_processing.py MsPacMan 0
--- run_1.sh
+++ run_1.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 
-for seed in 0 1 2
+for seed in 1
 do
-    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc fabric.devices="[1]" seed=$seed algo.train_every=2 algo.cnn_keys.decoder="[]" algo.cnn_keys.encoder="[]"
+    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc fabric.devices="[1]" seed=$seed algo.train_every=2 buffer.size=10000
 done
-
-python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc fabric.devices="[1]" seed=1 algo.train_every=8 algo.cnn_keys.decoder="[]" algo.cnn_keys.encoder="[]"
-python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc fabric.devices="[1]" seed=1 algo.train_every=8 algo.mlp_keys.decoder="[]"
