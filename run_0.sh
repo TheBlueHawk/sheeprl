0d3be24daff5a6e33836ea6d34e0c7e3a2388233
Kevin Blin <kblin@outlook.com>
2024-03-01 11:21:07 +0100

    temp fix for test results viz

--- run_0.sh
+++ run_0.sh
@@ -3,6 +3,6 @@ DEVICE=0
 
 for seed in 0 1 2
 do
-    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc algo.cnn_keys.decoder=[] fabric.devices="[$DEVICE]" seed=$seed 
-    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc algo.cnn_keys.decoder=[] env.id=AssaultNoFrameskip-v4 fabric.devices="[$DEVICE]" seed=$seed
+    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc algo.train_every=2 algo.cnn_keys.decoder=[] fabric.devices="[$DEVICE]" seed=$seed 
+    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc algo.train_every=2 algo.cnn_keys.decoder=[] env.id=AssaultNoFrameskip-v4 fabric.devices="[$DEVICE]" seed=$seed
 done
