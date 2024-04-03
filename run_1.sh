d235cdedcfb876e375e2b6cae2779cc7ce4aff3f
Kevin Blin <kblin@outlook.com>
2024-04-03 13:03:33 +0200

    update viz, add folders for exta exp

--- run_1.sh
+++ run_1.sh
@@ -4,8 +4,10 @@ DEVICE=1
 
 for seed in 0 1 2
 do
-    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=AssaultNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=1
-    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=AssaultNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=2
+    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=CarnivalNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed algo.cnn_keys.decoder=[]
+    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=CarnivalNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed algo.cnn_keys.decoder=[] algo.cnn_keys.encoder=[]
 done
+
+
 
 
--- run_2.sh
+++ run_2.sh
@@ -4,6 +4,5 @@ DEVICE=2
 
 for seed in 0 1 2
 do
-    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=AssaultNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=3
-    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=AssaultNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=7
+    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=CarnivalNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed algo.mlp_keys.decoder=[] 
 done
