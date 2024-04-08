d2220490386208185197204938f8e5bb41eabc25
Kevin Blin <kblin@outlook.com>
2024-04-08 13:32:38 +0200

    update viz

--- run_1.sh
+++ run_1.sh
@@ -2,8 +2,13 @@
 
 DEVICE=1
 
-for seed in 1
+for seed in 4
 do
+    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=AssaultNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=1
+    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=AssaultNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=3
+    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=AssaultNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=2
+    python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc env.id=AssaultNoFrameskip-v4 algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=7
+
     python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=1
     python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=3
     python sheeprl.py exp=dreamer_v3_100k_ms_pacman_oc algo.train_every=2 fabric.devices="[$DEVICE]" seed=$seed env.wrapper.env.perturbation=2
